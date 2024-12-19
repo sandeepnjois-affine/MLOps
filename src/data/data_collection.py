b8bf41a5cde4d1a76e9129f7229b2fbe548c62d2
Sandeep N <sandeep.n@affine.ai>
2024-12-19 18:04:09 +0530

    experiment runs

--- src/data/data_collection.py
+++ src/data/data_collection.py
@@ -42,7 +42,7 @@ def save_data(df : pd.DataFrame, filepath: str) -> None:
     
 
 def main():
-    data_filepath = r"C:\Users\SFL-3\water_potability.csv"
+    data_filepath = "/home/affine/Downloads/water_potability.csv"
     params_filepath = "params.yaml"
     raw_data_path = os.path.join("data","raw")
 # data_path = os.path.join("data","raw")
